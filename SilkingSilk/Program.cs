b061cfb636ba83fd43c38ae8dc1ae0b1de75123d
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2023-02-03 10:39:44 -0300

    Shaders and some other implementations!

--- SilkingSilk/Program.cs
+++ SilkingSilk/Program.cs
@@ -2,18 +2,34 @@
 using Silk.NET.Maths;
 using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
-using System.Drawing;
 
 namespace SilkingSilk
 {
     internal class Program
     {
         private static IWindow _window;
-        private static GL _gl;
-        private static uint _vao;
-        private static uint _vbo;
-        private static uint _ebo;
-        private static uint _program;
+        private static GL Gl;
+
+
+        //Our new abstracted objects, here we specify what the types are.
+        private static BufferObject<float> Vbo;
+        private static BufferObject<uint> Ebo;
+        private static VertexArrayObject<float, uint> Vao;
+        private static SilkingSilk.Shader Shader;
+        private static readonly float[] Vertices =
+        {
+            //X    Y      Z     R  G  B  A
+             0.5f,  0.5f, 0.0f, 1, 0, 0, 1,
+             0.5f, -0.5f, 0.0f, 0, 0, 0, 1,
+            -0.5f, -0.5f, 0.0f, 0, 0, 1, 1,
+            -0.5f,  0.5f, 0.5f, 0, 0, 0, 1
+        };
+
+        private static readonly uint[] Indices =
+{
+            0, 1, 3,
+            1, 2, 3
+        };
 
         private static void Main(string[] args)
         {
@@ -23,11 +39,12 @@ namespace SilkingSilk
                 Title = "My first Silk.NET application!"
             };
 
-            _window = Window.Create(options);
+            _window = Silk.NET.Windowing.Window.Create(options);
 
             _window.Load += OnLoad;
             _window.Update += OnUpdate;
             _window.Render += OnRender;
+            _window.Closing += OnClose;
 
             _window.Run();
         }
@@ -35,106 +52,20 @@ namespace SilkingSilk
         private static unsafe void OnLoad()
         {
             IInputContext input = _window.CreateInput();
-            _gl = _window.CreateOpenGL();
-            _gl.ClearColor(Color.CornflowerBlue);
-
-            OpenGlCode();
-
-            _gl.BindVertexArray(_vao);
-            _gl.UseProgram(_program);
-            _gl.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, (void*)0);
             for (int i = 0; i < input.Keyboards.Count; i++)
                 input.Keyboards[i].KeyDown += KeyDown;
-        }
 
-        private static unsafe void OpenGlCode()
-        {
-            _vao = _gl.GenVertexArray();
-            _gl.BindVertexArray(_vao);
-            float[] vertices =
-            {
-                0.5f,  0.5f, 0.0f,
-                0.5f, -0.5f, 0.0f,
-                -0.5f, -0.5f, 0.0f,
-                -0.5f,  0.5f, 0.0f
-            };
-            _vbo = _gl.GenBuffer();
-            _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vbo);
+            Gl = GL.GetApi(_window);
+            //Instantiating our new abstractions
+            Ebo = new BufferObject<uint>(Gl, Indices, BufferTargetARB.ElementArrayBuffer);
+            Vbo = new BufferObject<float>(Gl, Vertices, BufferTargetARB.ArrayBuffer);
+            Vao = new VertexArrayObject<float, uint>(Gl, Vbo, Ebo);
 
-            fixed (float* ptr = vertices)
-                _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(vertices.Length * sizeof(float)), ptr, BufferUsageARB.StaticDraw);
+            //Telling the VAO object how to lay out the attribute pointers
+            Vao.VertexAttributePointer(0, 3, VertexAttribPointerType.Float, 7, 0);
+            Vao.VertexAttributePointer(1, 4, VertexAttribPointerType.Float, 7, 3);
 
-            uint[] indices =
-            {
-                0u, 1u, 3u,
-                1u, 2u, 3u
-            };
-
-            _ebo = _gl.GenBuffer();
-            _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, _ebo);
-            fixed (uint* buf = indices)
-                _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint)(indices.Length * sizeof(uint)), buf, BufferUsageARB.StaticDraw);
-
-            const string vertexCode = @"
-                #version 330 core
-
-                layout (location = 0) in vec3 aPosition;
-
-                void main()
-                {
-                    gl_Position = vec4(aPosition, 1.0);
-                }";
-
-            const string fragmentCode = @"
-                #version 330 core
-
-                out vec4 out_color;
-
-                void main()
-                {
-                    out_color = vec4(1.0, 0.5, 0.2, 1.0);
-                }";
-
-            uint vertexShader = _gl.CreateShader(ShaderType.VertexShader);
-            _gl.ShaderSource(vertexShader, vertexCode);
-            _gl.CompileShader(vertexShader);
-
-            _gl.GetShader(vertexShader, ShaderParameterName.CompileStatus, out int vStatus);
-            if (vStatus != (int)GLEnum.True)
-                throw new Exception("Vertex shader failed to compile: " + _gl.GetShaderInfoLog(vertexShader));
-
-            uint fragmentShader = _gl.CreateShader(ShaderType.FragmentShader);
-            _gl.ShaderSource(fragmentShader, fragmentCode);
-
-            _gl.CompileShader(fragmentShader);
-
-            _gl.GetShader(fragmentShader, ShaderParameterName.CompileStatus, out int fStatus);
-            if (fStatus != (int)GLEnum.True)
-                throw new Exception("Fragment shader failed to compile: " + _gl.GetShaderInfoLog(fragmentShader));
-
-            _program = _gl.CreateProgram();
-
-            _gl.AttachShader(_program, vertexShader);
-            _gl.AttachShader(_program, fragmentShader);
-
-            _gl.LinkProgram(_program);
-
-            _gl.GetProgram(_program, ProgramPropertyARB.LinkStatus, out int lStatus);
-            if (lStatus != (int)GLEnum.True)
-                throw new Exception("Program failed to link: " + _gl.GetProgramInfoLog(_program));
-
-            _gl.DetachShader(_program, vertexShader);
-            _gl.DetachShader(_program, fragmentShader);
-            _gl.DeleteShader(vertexShader);
-            _gl.DeleteShader(fragmentShader);
-
-            const uint positionLoc = 0;
-            _gl.EnableVertexAttribArray(positionLoc);
-            _gl.VertexAttribPointer(positionLoc, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), (void*)0);
-
-            _gl.BindVertexArray(0);
-            _gl.BindBuffer(BufferTargetARB.ArrayBuffer, 0);
-            _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, 0);
+            Shader = new SilkingSilk.Shader(Gl, "shader.vert", "shader.frag");
         }
 
         private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
@@ -148,7 +79,25 @@ namespace SilkingSilk
 
         private static unsafe void OnRender(double dt)
         {
-            _gl.Clear(ClearBufferMask.ColorBufferBit);
+            Gl.Clear((uint)ClearBufferMask.ColorBufferBit);
+
+            //Binding and using our VAO and shader.
+            Vao.Bind();
+            Shader.Use();
+            //Setting a uniform.
+            Shader.SetUniform("uBlue", (float)Math.Sin(DateTime.Now.Millisecond / 1000f * Math.PI));
+
+            Gl.DrawElements(PrimitiveType.Triangles, (uint)Indices.Length, DrawElementsType.UnsignedInt, null);
+
+        }
+
+        private static void OnClose()
+        {
+            //Remember to dispose all the instances.
+            Vbo.Dispose();
+            Ebo.Dispose();
+            Vao.Dispose();
+            Shader.Dispose();
         }
     }
 }
